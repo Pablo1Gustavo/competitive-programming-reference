7273903e87cfdc01df9a4ab68a06286e6d03a541
lucasaamorim <lucas.apolonio.125@ufrn.edu.br>
2024-09-11 19:23:12 -0300

    Add Number theory related cpp snippets
    Includes snippets for: Binary Exponentiation modulo m, Sieve of Eratosthenes, Extended Euclidean algorithm, Prime Factorization and Modular Multiplicative Inverse
    All files were tested in real problems and worked, comments include a description of what the snippet does and time complexity for the algorithm.

---
+++ number theory/factorization.h
@@ -0,0 +1,30 @@
+/**
+ * Description: Returns all prime factors of a number by using precomputed primes.
+ * Time: $O(\sqrt{MAX} \log \log \sqrt{MAX})$ for computing the necessary primes and $O(\log N)$ per query.
+ * Status: Tested
+ */
+#pragma once
+
+#include "./eratosthenes.h"
+
+int safe_sqrt(int x) {
+  int sq = max((int)(sqrt(x)-1),0);
+  while(sq*sq < x) sq++;
+  return sq;
+}
+
+const int MAX = 1e6;
+vector<int> primes = eratosthenes(safe_sqrt(MAX));
+
+vector<int> factorization(int n) {
+  vector<int> factors;
+  for (auto &p : primes) {
+    if (p*p > n) break;
+    while(n%p == 0) {
+      factors.push_back(p);
+      n/=p;
+    }
+  }
+  if (n > 1) factors.push_back(n);
+  return factors;
+}
